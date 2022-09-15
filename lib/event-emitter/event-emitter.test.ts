d1f96602b93db2cb4b39b7f32d85dac5953f1e6f
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-15 16:00:14 +0000

    test: add tests for EventEmitter event observation

--- lib/event-emitter/event-emitter.test.ts
+++ lib/event-emitter/event-emitter.test.ts
@@ -10,3 +10,34 @@ describe('EventEmitter instantiation', () => {
     expect(eventEmitter.events).toEqual(['click', 'focus']);
   });
 });
+
+describe('EventEmitter event observation', () => {
+  it('should add an event observer', () => {
+    let eventEmitted = false;
+    const eventEmitter = new EventEmitter({}, ['click', 'focus']);
+
+    eventEmitter.addObserver('click', () => {
+      eventEmitted = true;
+    });
+
+    eventEmitter.emit('click');
+    expect(eventEmitted).toBe(true);
+  });
+
+  it('should remove an event observer', () => {
+    let eventEmitted = false;
+    const eventEmitter = new EventEmitter({}, ['click', 'focus']);
+
+    const eventObserver = () => {
+      eventEmitted = true;
+    };
+
+    eventEmitter.addObserver('click', eventObserver);
+    eventEmitter.emit('click');
+    expect(eventEmitted).toBe(true);
+    eventEmitter.removeObserver('click', eventObserver);
+    eventEmitted = false;
+    eventEmitter.emit('click');
+    expect(eventEmitted).toBe(false);
+  });
+});
