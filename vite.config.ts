eb6931b978e39faaaf6f4c858abea6187094c481
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-15 16:18:56 +0000

    build: use vite-plugin-dts in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
+      entry: path.resolve(__dirname, 'lib/main.ts'),
       name: 'event-manager',
       fileName: (format) => `event-manager.${format}.js`
     }
-  }
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true
+    })
+  ]
 });
