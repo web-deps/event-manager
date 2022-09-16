b22536bbf639dfd3941c82b6f7ab443cee634d06
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-16 09:34:58 +0000

    feat: add main module - The main module exports API content.

---
+++ lib/main.ts
@@ -0,0 +1,7 @@
+export { default as Event } from "./event/event";
+export type { EventInterface } from "./event/event";
+export { default as EventEmitter } from "./event-emitter/event-emitter";
+export type {
+  AbstractEventEmitter,
+  EventObserverType
+} from "./event-emitter/event-emitter";
